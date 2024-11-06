0a6f2fd126bb2e4ead26a019f7e81d614e8565ef
Devil-jai <140802260+Devil-jai@users.noreply.github.com>
2024-11-06 13:29:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(cors({
     // origin: "https://entertainment-1-jcso.onrender.com",
-    origin:"http://localhost:3000",
+    origin:"https://entertainment-app-vercel-m24q.vercel.app/",
 
     methods: ['GET', 'POST', 'PUT', 'DELETE' ],
     credentials: true,
